070999b04c35163cbc7e217556b357c01590aceb
starwort <tcphone93@gmail.com>
2023-05-16 11:39:04 +0100

    (slightly janky) live preview

--- bfh.js
+++ bfh.js
@@ -1,17 +1,87 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var p = document.getElementById('text-p');
 var btn = document.querySelector('button');
 var subheading = document.getElementById('subheading');
 if (subheading) {
     subheading.innerHTML = bfh(subheading.innerText);
 }
+var sel = document.getSelection();
 var makeBFH = function () {
     if (p) {
         p.innerHTML = bfh(p.innerText);
     }
 };
 btn === null || btn === void 0 ? void 0 : btn.addEventListener('click', makeBFH);
-// not working
-// p?.addEventListener('keyup', makeBFH);
+function findNodeOffset(nodes, node) {
+    var _a, _b, _c;
+    console.log('findNodeOffset(', nodes, node, ')');
+    var total = 0;
+    for (var _i = 0, _d = nodes !== null && nodes !== void 0 ? nodes : []; _i < _d.length; _i++) {
+        var childNode = _d[_i];
+        if (childNode.nodeName === 'BR') {
+            total += 1;
+        }
+        if (childNode === node || childNode === node.parentNode) {
+            console.log('  =>', total);
+            return total;
+        }
+        else {
+            total += (_b = (_a = childNode.textContent) === null || _a === void 0 ? void 0 : _a.length) !== null && _b !== void 0 ? _b : 0;
+        }
+    }
+    // seems like if we get here we've got the parent <p> element
+    return (_c = nodes === null || nodes === void 0 ? void 0 : nodes.length) !== null && _c !== void 0 ? _c : 0;
+}
+function findOffsetNode(nodes, offset) {
+    var _a, _b;
+    console.log('findOffsetNode(', nodes, offset, ')');
+    var lengths = __spreadArray([], nodes !== null && nodes !== void 0 ? nodes : [], true).map(function (node) { var _a; return (_a = node.textContent) === null || _a === void 0 ? void 0 : _a.length; });
+    var total2 = 0;
+    console.log(' ...', lengths, lengths.map(function (len) { total2 += len; return total2 - len; }));
+    var total = 0;
+    for (var _i = 0, _c = nodes !== null && nodes !== void 0 ? nodes : []; _i < _c.length; _i++) {
+        var childNode = _c[_i];
+        var nodeLen = 0;
+        if (childNode.nodeName === 'BR') {
+            offset -= 1;
+        }
+        else {
+            nodeLen = (_b = (_a = childNode.textContent) === null || _a === void 0 ? void 0 : _a.length) !== null && _b !== void 0 ? _b : 0;
+        }
+        if (total < offset - nodeLen) {
+            total += nodeLen;
+        }
+        else {
+            console.log('  =>', childNode, offset - total);
+            if (childNode.nodeName === 'STRONG') {
+                return [childNode.firstChild, offset - total];
+            }
+            else {
+                return [childNode, offset - total];
+            }
+        }
+    }
+    // if we fall off the end, make a new text node
+    var node = document.createTextNode('');
+    p === null || p === void 0 ? void 0 : p.appendChild(node);
+    return [node, 0];
+}
+p === null || p === void 0 ? void 0 : p.addEventListener('keyup', function (event) {
+    var node = sel === null || sel === void 0 ? void 0 : sel.anchorNode;
+    var offset = findNodeOffset(p.childNodes, node) + (sel === null || sel === void 0 ? void 0 : sel.focusOffset);
+    console.log();
+    makeBFH();
+    var _a = findOffsetNode(p.childNodes, offset), newNode = _a[0], newOffset = _a[1];
+    sel === null || sel === void 0 ? void 0 : sel.collapse(newNode, newOffset);
+});
 function bfh(str) {
     str = processNewlines(str);
     return str.replace(/(?<!<)(\b\w+)/g, processWord); // Convert newlines to <br> tags
--- bfh.ts
+++ bfh.ts
@@ -7,6 +7,8 @@ if (subheading) {
     subheading.innerHTML = bfh(subheading.innerText);
 }
 
+const sel = document.getSelection();
+
 const makeBFH = () => {
     if (p) {
         p.innerHTML = bfh(p.innerText);
@@ -15,8 +17,62 @@ const makeBFH = () => {
 
 btn?.addEventListener('click', makeBFH);
 
-// not working
-// p?.addEventListener('keyup', makeBFH);
+function findNodeOffset(nodes: NodeListOf<Node> | undefined, node: Node): number {
+    console.log('findNodeOffset(', nodes, node, ')');
+    let total = 0;
+    for (const childNode of nodes ?? []) {
+        if (childNode.nodeName === 'BR') {
+            total += 1;
+        }
+        if (childNode === node || childNode === node.parentNode) {
+            console.log('  =>', total);
+            return total;
+        } else {
+            total += childNode.textContent?.length ?? 0;
+        }
+    }
+    // seems like if we get here we've got the parent <p> element
+    return nodes?.length ?? 0;
+}
+
+function findOffsetNode(nodes: NodeListOf<ChildNode> | undefined, offset: number): [Node, number] {
+    console.log('findOffsetNode(', nodes, offset, ')');
+    let lengths = [...nodes ?? []].map((node) => node.textContent?.length!);
+    let total2 = 0;
+    console.log(' ...', lengths, lengths.map((len) => {total2 += len; return total2 - len;}));
+    let total = 0;
+    for (const childNode of nodes ?? []) {
+        let nodeLen = 0;
+        if (childNode.nodeName === 'BR') {
+            offset -= 1;
+        } else {
+            nodeLen = childNode.textContent?.length ?? 0;
+        }
+        if (total < offset - nodeLen) {
+            total += nodeLen;
+        } else {
+            console.log('  =>', childNode, offset - total);
+            if (childNode.nodeName === 'STRONG') {
+                return [childNode.firstChild!, offset - total];
+            } else {
+                return [childNode, offset - total];
+            }
+        }
+    }
+    // if we fall off the end, make a new text node
+    let node = document.createTextNode('');
+    p?.appendChild(node);
+    return [node, 0];
+}
+
+p?.addEventListener('keyup', (event) => {
+    const node = sel?.anchorNode!;
+    const offset = findNodeOffset(p!.childNodes, node) + sel?.focusOffset!;
+    console.log();
+    makeBFH();
+    const [newNode, newOffset] = findOffsetNode(p!.childNodes, offset);
+    sel?.collapse(newNode, newOffset);
+});
 
 
 function bfh(str: string): string {
